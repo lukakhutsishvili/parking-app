ab92ebe7052b70a8db7cd60d1e6ef2e1c37b459d
Luka Khutsishvili <luka.khutsishvili777@gmail.com>
2024-03-17 19:53:50 +0400

    project setUp

---
+++ src/config/sql.js
@@ -0,0 +1,15 @@
+import pgk from "pg";
+
+const { Pool } = pgk;
+
+const pool = new Pool({
+  host: "dpg-cnqqs2q1hbls73dseo60-a",
+  port: 5432,
+  database: "parking_app_n1t8",
+  user: "parking_app_n1t8_user",
+  password: "lh5bQVHIgKFO6gwukImMubAYHta01Kia",
+});
+
+export const createTable = async () => {
+  return await pool.query("CREATE IF NOT EXIST TABLE user(id SERIAL PRIMARY KEY, name TEXT, carType TEXT, password TEXT )");
+};
---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { createTable } from "./config/sql.js";
+
+const app = express();
+
+async function init() {
+  try {
+    await createTable();
+    serverStart;
+  } catch (error) {
+    console.log(error);
+  }
+
+  function serverStart() {
+    app.get("/", (req, res) => {
+      return res.status(201).json({ message: "works" });
+    });
+    app.listen(3000);
+  }
+}
+
+init();
